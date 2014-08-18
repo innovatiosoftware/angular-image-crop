6e2f71d3260510ace301e1c4de6ff6e73fc87f85
Edrick Ortiz <edrick.ortiz@gmail.com>
2014-08-18 18:33:08 -0400

    - missing dist stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,34 @@
  * Created by Edxe on 8/18/14.
  */
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify')
-concat = require('gulp-concat')
+    uglify = require('gulp-uglify');
+concat = require('gulp-concat');
 
-;
+var paths = {
+    scripts: ['*.js', '!gulpfile.js'],
+    styles: ['*.css'],
+    html: ['*.html','!index.html']
+};
 
-gulp.task('default', function () {
-    console.log('building...');
-    gulp.src(['*.js','!gulpfile.js'])
+
+gulp.task('default', ['scripts', 'styles', 'other']);
+
+gulp.task('scripts', function () {
+    console.log('processing scripts...');
+    gulp.src(paths.scripts)
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
+});
+
+gulp.task('styles', function () {
+    console.log('processing styles...');
+    gulp.src(paths.styles).pipe(gulp.dest('dist'))
+
+});
+
+gulp.task('other', function () {
+    console.log('processing other...');
+    gulp.src(paths.html).pipe(gulp.dest('dist'))
 });
 
 
